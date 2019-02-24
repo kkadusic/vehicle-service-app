942f9d6632feb850b75d23f2979d2e9e0d383770
kkadusic <kkadusic1@etf.unsa.ba>
2019-02-24 20:30:02 +0100

    Create new DTO class for vehicle parts

---
+++ src/ba/unsa/etf/rpr/projekat/Part.java
@@ -0,0 +1,65 @@
+package ba.unsa.etf.rpr.projekat;
+
+public class Part {
+    private int id;
+    private String brand;
+    private String model;
+    private String name;
+    private int quantity;
+
+    public Part(int id, String brand, String model, String name, int quantity) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public Part() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
