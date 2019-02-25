1b238d25adf7d98b24cc78bec2ee222b6e5deb44
kkadusic <kkadusic1@etf.unsa.ba>
2019-02-25 10:42:33 +0100

    Create controller class for parts

---
+++ src/ba/unsa/etf/rpr/projekat/PartController.java
@@ -0,0 +1,50 @@
+package ba.unsa.etf.rpr.projekat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class PartController {
+    private VehiclesDAO dao;
+    private Part part;
+
+    public TextField partBrandField;
+    public TextField partModelField;
+    public TextField partNameField;
+    public TextField partQuantityField;
+
+    public PartController() {
+        dao = null;
+        part = null;
+    }
+
+    public PartController(VehiclesDAO dao, Part part) {
+        this.dao = dao;
+        this.part = part;
+    }
+
+    @FXML
+    public void initialize() {
+        if (part != null) {
+            partBrandField.setText(part.getBrand());
+            partModelField.setText(part.getModel());
+            partNameField.setText(part.getName());
+            partQuantityField.setText(Integer.toString(part.getQuantity()));
+        }
+    }
+
+    public void okClick (ActionEvent actionEvent){
+        int quantity = Integer.parseInt(partQuantityField.getText());
+        if (part == null) {
+            part = new Part(0, partBrandField.getText(), partModelField.getText(), partNameField.getText(), quantity);
+            dao.addPart(part);
+        } else {
+            part.setBrand(partBrandField.getText());
+            part.setModel(partModelField.getText());
+            part.setName(partNameField.getText());
+            part.setQuantity(Integer.parseInt(partQuantityField.getText()));
+            dao.changePart(part);
+        }
+    }
+
+}
