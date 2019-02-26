439e7b2e12f0261570d1fdee8ff9a8b4f1d3f263
kkadusic <kkadusic1@etf.unsa.ba>
2019-02-26 14:21:00 +0100

    Create Service DTO and controller class + FXML

---
+++ src/ba/unsa/etf/rpr/projekat/Service.java
@@ -0,0 +1,60 @@
+package ba.unsa.etf.rpr.projekat;
+
+public class Service {
+    private int id;
+    private String vehicleIdNumber;
+    private String mechanicName;
+    private int inspectionsNumber;
+    private String details;
+
+    public Service(int id, String vehicleIdNumber, String mechanicName, int inspectionsNumber, String details) {
+        this.id = id;
+        this.vehicleIdNumber = vehicleIdNumber;
+        this.mechanicName = mechanicName;
+        this.inspectionsNumber = inspectionsNumber;
+        this.details = details;
+    }
+
+    public Service() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVehicleIdNumber() {
+        return vehicleIdNumber;
+    }
+
+    public void setVehicleIdNumber(String vehicleIdNumber) {
+        this.vehicleIdNumber = vehicleIdNumber;
+    }
+
+    public String getMechanicName() {
+        return mechanicName;
+    }
+
+    public void setMechanicName(String mechanicName) {
+        this.mechanicName = mechanicName;
+    }
+
+    public int getInspectionsNumber() {
+        return inspectionsNumber;
+    }
+
+    public void setInspectionsNumber(int inspectionsNumber) {
+        this.inspectionsNumber = inspectionsNumber;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
---
+++ src/ba/unsa/etf/rpr/projekat/ServiceController.java
@@ -0,0 +1,49 @@
+package ba.unsa.etf.rpr.projekat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ServiceController {
+    private VehiclesDAO dao;
+    private Service service;
+
+    public TextField serviceVehicleIdNumberField;
+    public TextField serviceMechanicNameField;
+    public TextField serviceInspectionsNumberField;
+    public TextField serviceDetailsField;
+
+    public ServiceController() {
+        dao = null;
+        service = null;
+    }
+
+    public ServiceController(VehiclesDAO dao, Service service) {
+        this.dao = dao;
+        this.service = service;
+    }
+
+    @FXML
+    public void initialize() {
+        if (service != null) {
+            serviceVehicleIdNumberField.setText(service.getVehicleIdNumber());
+            serviceMechanicNameField.setText(service.getMechanicName());
+            serviceInspectionsNumberField.setText(Integer.toString(service.getInspectionsNumber()));
+            serviceDetailsField.setText(service.getDetails());
+        }
+    }
+
+    public void okClick (ActionEvent actionEvent){
+        int inspections = Integer.parseInt(serviceInspectionsNumberField.getText());
+        if (service == null) {
+            service = new Service(0, serviceVehicleIdNumberField.getText(), serviceMechanicNameField.getText(), inspections, serviceDetailsField.getText());
+            dao.addService(service);
+        } else {
+            service.setVehicleIdNumber(serviceVehicleIdNumberField.getText());
+            service.setMechanicName(serviceMechanicNameField.getText());
+            service.setInspectionsNumber(Integer.parseInt(serviceInspectionsNumberField.getText()));
+            service.setDetails(serviceDetailsField.getText());
+            dao.changeService(service);
+        }
+    }
+}
