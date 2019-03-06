be185d85998e03f019d90b611e439515053690cd
kkadusic <kkadusic1@etf.unsa.ba>
2019-03-06 18:15:15 +0100

    Add user validation via database

--- src/ba/unsa/etf/rpr/projekat/LoginController.java
+++ src/ba/unsa/etf/rpr/projekat/LoginController.java
@@ -2,13 +2,12 @@ package ba.unsa.etf.rpr.projekat;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Optional;
+import java.sql.*;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
@@ -20,46 +19,41 @@ public class LoginController {
 
     Controller mainController;
 
+    private Connection conn = null;
+    private PreparedStatement getUsersQuery, getNewUserIdQuery, addUserQuery;
+
     public void openMainWindow(ActionEvent actionEvent) {
-        boolean loginSuccessful = validateLogin(usernameField, passwordField);
-        if (loginSuccessful) {
-            Parent root = null;
-            try {
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:resources/db/vehicle.db");
+            getUsersQuery = conn.prepareStatement("SELECT * FROM user WHERE username = ? AND password = ?");
+            getNewUserIdQuery = conn.prepareStatement("SELECT MAX(id)+1 FROM user");
+            addUserQuery = conn.prepareStatement("INSERT INTO user VALUES (?,?,?)");
+
+            getUsersQuery.setString(1, usernameField.getText());
+            getUsersQuery.setString(2, passwordField.getText());
+            ResultSet rs = getUsersQuery.executeQuery();
+
+            if (rs.next()) {
                 Stage myStage = new Stage();
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/screen.fxml"));
                 loader.load();
                 mainController = loader.getController();
-                myStage.setTitle("Main window");
+                myStage.setTitle("Main screen");
                 myStage.setScene(new Scene(loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
                 //myStage.getIcons().add(new Image("/img/car-icon.png"));
                 myStage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Invalid data");
+                alert.setHeaderText("The username or password you’ve \nentered doesn’t match any account");
+                alert.setContentText("Please enter new username and password");
+                alert.showAndWait();
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Invalid data");
-            alert.setHeaderText("The username or password you’ve \nentered doesn’t match any account");
-            alert.setContentText("Please enter new username and password");
-            Optional<ButtonType> result = alert.showAndWait();
-        }
-    }
-
-    private boolean validateLogin(TextField username, TextField password) {
-        if (username.getText().trim().isEmpty() || password.getText().trim().isEmpty()) {
-            username.getStyleClass().removeAll("fieldRight");
-            username.getStyleClass().add("fieldWrong");
-            password.getStyleClass().removeAll("fieldRight");
-            password.getStyleClass().add("fieldWrong");
-            return false;
-        } else {
-            username.getStyleClass().removeAll("fieldWrong");
-            username.getStyleClass().add("fieldRight");
-            password.getStyleClass().removeAll("fieldWrong");
-            password.getStyleClass().add("fieldRight");
-        }
-        return true;
     }
 
 }
